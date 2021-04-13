e9f577b87a9e4cbd85a873fdd40c6e3139fb40b6
wu <jianhengwu0407@gmail.com>
2021-04-13 22:17:30 +0800

    'form_and_class_view' end.

--- HelloWorld/polls/urls.py
+++ HelloWorld/polls/urls.py
@@ -3,14 +3,13 @@ from django.urls import path
 from . import views
 
 app_name = 'polls'
-
 urlpatterns = [
     # this is a test
-    path('', views.index, name='index'),
+    path('', views.IndexView.as_view(), name='index'),
     # 详细文本内容
-    path('<int:question_id>/', views.detail, name="detail"),
+    path('<int:pk>/', views.DetailView.as_view(), name="detail"),
     # 问卷投票或调查结果
-    path('<int:question_id>/results/', views.results, name='results'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
     # 投票动作
     path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
--- HelloWorld/polls/views.py
+++ HelloWorld/polls/views.py
@@ -1,45 +1,47 @@
 from django.shortcuts import render,get_object_or_404
-
-# Create your views here.
-
 from django.http import HttpResponse
 from django.template import loader
 from django.http import Http404
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
+from django.views import generic
 
 from .models import Question,Choice
 
 # def index(request):
 #     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-#     template = loader.get_template('polls/index.html')
 #     context = {
 #         'latest_question_list':latest_question_list,
 #     }
-#     return HttpResponse(template.render(context, request))
-
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {
-        'latest_question_list':latest_question_list,
-    }
-    return render(request, 'polls/index.html', context)
-
-# 详细文本内容
+#     return render(request, 'polls/index.html', context)
+#
 # def detail(request, question_id):
-#     try:
-#         question = Question.objects.get(pk=question_id)
-#     except Question.DoesNotExist:
-#         raise Http404("Question does not exist \n HTTP-404")
+#     question = get_object_or_404(Question, pk=question_id)
 #     return render(request, 'polls/detail.html', {'question': question})
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html', {'question': question})
+#
+# # 问卷投票或调查结果
+# def results(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, 'polls/results.html', {'question': question})
 
-# 问卷投票或调查结果
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/results.html', {'question': question})
+# 类视图
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
+
+# 通用视图
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+# 通用视图
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
 
 # 投票动作
 def vote(request, question_id):
