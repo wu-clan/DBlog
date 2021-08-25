e2195002c24aff5af1b04ae8da9362d15c8425d5
wu <jianhengwu0407@gmail.com>
2021-08-26 01:46:25 +0800

    add comment forms.

---
+++ blog/comment/forms.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from django import forms
+
+from blog.models import Comment
+
+
+class CommentForm(forms.ModelForm):
+	"""
+	评论扩展表单
+	"""
+	class Meta:
+		model = Comment
+		fields = (
+			'user_name',
+			'email',
+			'url',
+			'comment'
+		)
